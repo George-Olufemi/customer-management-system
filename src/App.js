55336754c91b82d724789636935fb8ac26ee4487
George Olufemi <georgeoluolufemi@gmail.com>
2022-05-12 22:46:51 +0100

    installed Tailwind and Installed React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from './LandingPage';
+import SignUp from './SignUp';
+import Login from './Login';
+import Dashboard from './Dashboard';  
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route to="/" element={<LandingPage />} />
+        <Route to="/signUp" element={<SignUp />} />
+        <Route to="/login" element={<Login />} />
+        <Route to="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
