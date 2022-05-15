6be57e345fefaa4cbd7cc15dab4cd873a7725e2a
George Olufemi <georgeoluolufemi@gmail.com>
2022-05-15 14:33:17 +0100

    Clean up

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function Login() {
-    const successToast = () => {
-        toast("success custom toast", {
-            className: 'custom-toast',
-            draggable: true,
-            position: toast.POSITION.BOTTOM_CENTER
-        });
-    }
   return (
     <React.Fragment>
         <div>
@@ -24,7 +16,7 @@ function Login() {
         </Link>
     </div>
         <div className='flex justify-center items-center mt-20 px-10 md:px-0'>
-        <form class="w-full max-w-sm">
+        <form  class="w-full max-w-sm">
             <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3">
                 <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
@@ -49,12 +41,11 @@ function Login() {
             <div class="md:flex md:items-center">
                 <div class="md:w-1/3"></div>
                 <div class="md:w-2/3">
-                <Link to="/dashboard">
-                    <button onClick={successToast} class="shadow bg-black hover:bg-gray-800 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded-lg hover:scale-105 transition-all hover:shadow-2xl" type="button">
+                {/* <Link to="/dashboard"> */}
+                    <button class="shadow bg-black hover:bg-gray-800 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded-lg hover:scale-105 transition-all hover:shadow-2xl" type="button">
                         Login
                     </button>
-                    <ToastContainer />
-                </Link>
+                {/* </Link> */}
                 </div>
             </div>
             </form>
