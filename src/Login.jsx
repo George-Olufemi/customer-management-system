03ad729d819971e1220460f36d55e3d1026c7bd9
George Olufemi <georgeoluolufemi@gmail.com>
2022-05-17 16:32:12 +0100

    modified the Product Page

--- src/Login.jsx
+++ src/Login.jsx
@@ -7,14 +7,14 @@ function Login() {
     <React.Fragment>
         <div>
             <div className='mt-5 ml-8'>
-        <Link to="/">
-            <div>
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:scale-110" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                <path strokeLinecap="round" strokeLinejoin="round" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
-                </svg>
-            </div>
-        </Link>
-    </div>
+                <Link to="/">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:scale-110" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+                        </svg>
+                    </div>
+                </Link>
+        </div>
         <div className='flex justify-center items-center mt-20 px-10 md:px-0'>
         <form  class="w-full max-w-sm">
             <div class="md:flex md:items-center mb-6">
--- src/Product.jsx
+++ src/Product.jsx
@@ -1,12 +1,12 @@
-
+import { Link } from 'react-router-dom'
 import './Product.css';
 const data = [
   { trackingid: '1', product: "Giordani Gold Foundation", customer: "Odunayo", date: "14 May",amount: '10,500', paymentmethod: "Online Payment", status: "Approved" },
   { trackingid: '2', product: "Matte Lipstick", customer: "Prudence", date: "14 May",amount: '7,000', paymentmethod: "Online Payment", status: "Approved" },
-  { trackingid: '3', product: "Power Face Powder", customer: "Esther", date: "14 May",amount: '5500', paymentmethod: "Cash Payment", status: "Approved" },
+  { trackingid: '3', product: "Power Face Powder", customer: "Esther", date: "14 May",amount: '5,500', paymentmethod: "Cash Payment", status: "Approved" },
   { trackingid: '4', product: "Novage Men Set", customer: "George", date: "15 May",amount: '35,500', paymentmethod: "Online Payment", status: "Pending" },
-  { trackingid: '5', product: "North For Men Roll-on", customer: "Ariyo", date: "15 May",amount: '1,950', paymentmethod: "Cash Payment", status: "Approved" },
-  { trackingid: '6', product: "The One Eyeliner", customer: "Esther", date: "15 May",amount: '5,00', paymentmethod: "Cash Payment", status: "Approved" },
+  { trackingid: '5', product: "North For Men Roll-on", customer: "Ariyo", date: "15 May",amount: '15,950', paymentmethod: "Cash Payment", status: "Approved" },
+  { trackingid: '6', product: "The One Eyeliner", customer: "Esther", date: "15 May",amount: '5,000', paymentmethod: "Cash Payment", status: "Approved" },
   { trackingid: '7', product: "Eclat Style Parfum", customer: "Paul", date: "16 May",amount: '24,500', paymentmethod: "Online Payment", status: "Pending" },
   { trackingid: '8', product: "Novage SPF50", customer: "Rella", date: "16 May",amount: '12,000', paymentmethod: "Cash Payment", status: "Approved" },
   { trackingid: '9', product: "Love Nature Toner", customer: "Princess", date: "16 May",amount: '5,500', paymentmethod: "Cash Payment", status: "Approved" },
@@ -21,38 +21,50 @@ const data = [
 
 ]
 
-function App() {
+function Product() {
   return (
-    <div className='App'>
-    <table>
-      <tr>
-        <th> ID </th>
-        <th> Product </th>
-        <th> Customer </th>
-        <th> Date </th>
-        <th> Amount </th>
-        <th> Payment </th>
-        <th> Status </th>
-      </tr>
-      {data.map((val, key) => {
-        return (
-          <tr key={key}>
-            <td className='Tablecell'> { val.trackingid} </td>
-            <td  className='Tablecell'> { val.product }</td>
-            <td  className='Tablecell'> { val.customer } </td>
-            <td  className='Tablecell'> { val.date } </td>
-            <td  className='Tablecell'> { val.amount } </td>
-            <td  className='Tablecell'> { val.paymentmethod } </td>
-            <td  className='Tablecell'> { val.status } </td>
-            
-          </tr>
-        )
-      })}
-    </table>
-
-  </div>
-  
+    <div className="bg-gray-200">
+      <div className='pt-6 ml-8'>
+                <Link to="/dashboard">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:scale-110" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+                        </svg>
+                    </div>
+                </Link>
+        </div>
+        <div className='md:absolute md:right-20 md:top-16'>
+          <button className="bg-green-500 text-white py-2 px-4 font-semibold rounded-lg tracking-wider hover:bg-green-600 hover:scale-110 transition-all">Add New</button>
+        </div>
+          <div className='App'>
+            <table className="">
+              <tr>
+                <th> ID </th>
+                <th> Product </th>
+                <th> Customer </th>
+                <th> Date </th>
+                <th> Amount </th>
+                <th> Payment </th>
+                <th> Status </th>
+              </tr>
+              {data.map((val, key) => {
+                return (
+                  <tr className="divide-x-2 divide-black border-b border-black" key={key}>
+                    <td className='Tablecell'> { val.trackingid} </td>
+                    <td  className='Tablecell bg-blue-200'> { val.product }</td>
+                    <td  className='Tablecell bg-orange-200'> { val.customer } </td>
+                    <td  className='Tablecell bg-red-200'> { val.date } </td>
+                    <td  className='Tablecell bg-violet-200'> { val.amount } </td>
+                    <td  className='Tablecell bg-green-300'> { val.paymentmethod } </td>
+                    <td  className='Tablecell bg-gray-400 text-white'> { val.status } </td>
+                    
+                  </tr>
+                )
+              })}
+            </table>
+          </div>
+    </div>
   );
 }
 
-export default App;
+export default Product;
