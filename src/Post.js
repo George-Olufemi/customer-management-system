f26c23de68f2e1b8e84b597bfe3c6c0d3f928d9d
George Olufemi <georgeoluolufemi@gmail.com>
2022-05-19 21:16:14 +0100

    Worked on Post Page

--- src/Post.js
+++ src/Post.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import './Post.css';
+import './post.css';
 import { Link } from "react-router-dom";
 
 const Post = () => {
     
     return (
-        <div  className="bg-gray-200">
-<div className='pt-6 ml-8'>
+        <div  className="">
+            <div className='pt-6 ml-8'>
                 <Link to="/dashboard">
                     <div>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:scale-110" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
@@ -15,63 +15,53 @@ const Post = () => {
                     </div>
                 </Link>
         </div>
-        <div class="container">
+        <center>
+        <div class="container flex justify-center items-center">
             <form class="row" >
-
-                <h3><u>Add New Product Info</u></h3>
-
+                <h3 className='text-xl pb-5'><u>Add New Product Info</u></h3>
                 <div >
-                    <label  for="Product">
+                    <label for="Product">
                         Product
                     </label>
-                    <br/>
-                    <input   type="text" required placeholder="Product" name="Product"  /> 
+                    <input className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-black' type="text" required placeholder="Product" name="Product"  /> 
                 </div> 
-                <br/>
-
                 <div>
                     <label for="Customers">
                         Customers
                     </label>
                     <br/>
-                    <input  type="text" required placeholder="Customer" name="Customer"  /> 
+                    <input className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-black' type="text" required placeholder="Customer" name="Customer"  /> 
                 </div> 
-                <br/>
                 <div >
-                    <label  for="Amount">
+                    <label className='' for="Amount">
                         Amount
                     </label>
-                    <br/>
-                    <input  type="text" required placeholder="Amount" name="Amount"  />
+                    <input className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-black' type="text" required placeholder="Amount" name="Amount"  />
                 </div>
-                <br/>
                 <div >
                     <label  for="Payment Type">
                         Payment type
                     </label>
                     <br/>
-                    <input type="text" required placeholder="Payment Type" name="Payment"  /> 
+                    <input className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-black' type="text" required placeholder="Payment Type" name="Payment"  /> 
                 </div>
-                <br/>
                 <div >
                     <label for="status">
                         Status
                     </label>
                     <br/>
-                    <input type="text" required placeholder="Status" name="Status"  />  
+                    <input className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-black' type="text" required placeholder="Status" name="Status"  />  
                 </div>
-                <br/>
                 <div class="">
-                <Link to="./Product.jsx">
-                    <button  type="submit">
+                <Link to="/product">
+                    <button className='text-white text-xl font-semibold mt-10 px-4 py-2 bg-black' type="submit">
                     Post
                     </button>
                 </Link>
                 </div>
-                
-                
             </form>
         </div>
+        </center>
     </div>  
  );
 }
