53df5c1da041bc1a16305f346b2d8425297d8eb3
George Olufemi <georgeoluolufemi@gmail.com>
2022-05-19 18:54:16 +0100

    Worked on Dashboard

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { motion } from 'framer-motion';
+import dash from './dash/dash.png';
+// import img1 from './img1/stat1.png';
+// import img2 from './img2/stat2.png';
+// import img3 from './img3/stat3.png';
 
 function Dashboard() {
   return (
     <React.Fragment>
       <motion.div 
-        className='relative'
+        className='md:flex'
         initial={{ opacity: 0, width: 0 }}
         animate={{ opacity: 1, width: "100%" }}
         exit={{ opacity: 0, x: window.innerWidth, transition: { duration:0.6 } }}
@@ -29,9 +33,24 @@ function Dashboard() {
                   <h1 className="text-white text-lg mb-16">Logout</h1>
               </Link>
         </div>
-        {/* <div class="absolute left-96">
-          <h1 className="text-black text-lg mb-16">Home</h1>
-        </div> */}
+        <div class="">
+          <h1 className="text-black text-3xl font-bold pt-9 pl-10">Welcome</h1>
+          <h1 className="text-black text-lg pl-10">You are logged in as <span className="text-blue-500">admin</span></h1>
+          <div>
+            <img className='px-5 pt-5' src={dash} alt="dashboard graph" />
+            {/* <div className='flex'>
+              <div>
+                <img src={img1} alt="" />
+              </div>
+              <div>
+                <img src={img2} alt="" />
+              </div>
+              <div>
+                <img src={img3} alt="" />
+              </div>
+            </div> */}
+          </div>
+        </div>
       </motion.div>
     </React.Fragment>
   )
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -28,16 +28,16 @@ const Body = () => {
               </Slide>
             {/* Text Area */}
             <div>
-                <div className="text-white text-4xl md:text-7xl font-bold tracking-wider leading-normal pt-10 md:pt-20 text-center">
+                <div className="text-white text-4xl md:text-7xl font-bold tracking-wider leading-normal pt-10 md:pt-16 text-center">
                   <h1 className="leading-none px-5 md:px-1">Let's make management and<br /> customer relationship<br /> <span className="underline">easy.</span></h1>
                 </div>
                 <div className="text-white pt-16 text-semibold text-lg px-5 md:text-3xl text-center">
                   <h1 className="leading-snug">We offer free and exclusive services with limited time offers.<br /> We have new and special offers with 70% off for you to save 30%<br /> of your money with great and outstanding services.<br /> Start today by clicking on the button below, you get free crypto-tokens<br /> and coupons on signup.</h1>
                 </div>
             </div>
-              <div className='text-center flex justify-center items-center mt-12 text-2xl'>
+              <div className='text-center flex justify-center items-center mt-10'>
                   <Link to="/signup">
-                      <button className="bg-black text-white text-2xl px-3 py-1 md:px-4 md:py-2 rounded-lg tracking-wider md:font-semibold hover:bg-gray-900 hover:text-white hover:scale-110 transition-all mb-16">Get Started</button>
+                      <button className="bg-black text-white text-xl py-2 rounded-lg tracking-wider md:font-semibold hover:bg-gray-900 hover:text-white hover:scale-110 transition-all">Get Started</button>
                   </Link>
               </div>
           </div>
